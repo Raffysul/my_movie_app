5a5f0dd1f42c6ac28c2bfb6ab0bbeb969f1a7187
Sulaimon Rafiat <raffysul1@gmail.com>
2023-03-14 10:03:46 +0100

    create the remoteservice class

--- lib/services/remote_servces.dart
+++ lib/services/remote_servces.dart
@@ -1,19 +1,25 @@
-// import 'package:http/http.dart' as http;
-//
-// import '../models/movies.dart';
-//
-// class RemoteService {
-//   Future<List<Movies>?> getMovies() async {
-//     var client = http.Client();
-//     var uri = Uri.parse("https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr");
-//     //final uri = Uri.parse(url);
-//     var response = await client.get(uri);
-//     if (response.statusCode == 200) {
-//       var json = response.body;
-//       return moviesFromJson(json);
-//
-//
-//     }
-//     return null;
+import 'package:http/http.dart' as http;
+
+import '../models/movies.dart';
+
+class RemoteService {
+  Future<Movies?> getMovies() async {
+    var client = http.Client();
+    var uri = Uri.parse("https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr");
+    //final uri = Uri.parse(url);
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return moviesFromJson(json);
+    }
+    return null;
+  }
+}
+// Future<List<Movies>> _fetchAllMovies() async {
+//   final response = await http.get("https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr");
+//   if (response.statusCode == 200) {
+//     final result = jsonDecode(response.body);
+//     Iterable list = result["d"];
+//     return list.map((movies) => Movie.fromJson)
 //   }
 // }
