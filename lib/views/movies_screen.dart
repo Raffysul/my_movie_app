8d457a8132f3d6dd4b81d0b033052786168c589c
Raffysul <raffysul1@gmail.com>
2023-03-23 02:41:57 +0100

    Designing the movies screen

--- lib/views/movies_screen.dart
+++ lib/views/movies_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_movie_app/views/home_page%20.dart';
 
 class MoviesScreen extends StatefulWidget {
   const MoviesScreen({Key? key}) : super(key: key);
@@ -10,6 +11,44 @@ class MoviesScreen extends StatefulWidget {
 class _MoviesScreenState extends State<MoviesScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: const BoxDecoration(color: Colors.white),
+              //height: 240,
+              child: Stack(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/image2.png',
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    top: 30,
+                    left: 20,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()),
+                        );
+                      },
+                      child: const Icon(
+                        Icons.arrow_back_ios,
+                        size: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
