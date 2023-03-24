90cf89b5f215355e4ef6c03a3c10b5ba4ba8f6ac
Raffysul <raffysul1@gmail.com>
2023-03-24 02:57:53 +0100

    Designing the main movie screen

--- lib/views/movies_screen.dart
+++ lib/views/movies_screen.dart
@@ -10,13 +10,16 @@ class MoviesScreen extends StatefulWidget {
 }
 
 class _MoviesScreenState extends State<MoviesScreen> {
+  var list = ["Action", "Biography", "Drama"];
 
   @override
   void initState() {
     // TODO: implement initState
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +28,12 @@ class _MoviesScreenState extends State<MoviesScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(
-              decoration: const BoxDecoration(color: Colors.white),
-              //height: 240,
+            SizedBox(
+              height: 325,
+              //decoration: const BoxDecoration(color: Colors.white),
+              //height: 30,
               child: Stack(
+                fit: StackFit.loose,
                 children: <Widget>[
                   Image.asset(
                     'assets/images/image2.png',
@@ -52,13 +57,209 @@ class _MoviesScreenState extends State<MoviesScreen> {
                     ),
                   ),
                   Positioned(
+                    bottom: 0,
+                    left: 30,
+                    width: 400,
+                    height: 100,
                     child: Container(
-                      decoration: BoxDecoration()
-                    ),
+                        // width: 100,
+                        // height: 100,
+                        margin: const EdgeInsets.all(15),
+                        padding: const EdgeInsets.all(8),
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.horizontal(
+                                left: Radius.circular(30))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(Icons.star, color: Color(0xFFFCC419)),
+                                Text(
+                                  '8.2/10',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  '150,212',
+                                  style: TextStyle(
+                                      color: Color(0xFF9A9BB2),
+                                      fontSize: 6,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(Icons.star_border),
+                                Text(
+                                  'Rate This',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  color: Colors.green,
+                                  child: const Text(
+                                    '86',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                                const Text(
+                                  'Metascore',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                const Text(
+                                  '52 critic reviews',
+                                  style: TextStyle(
+                                      color: Color(0xFF9A9BB2),
+                                      fontSize: 6,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
                   ),
                 ],
               ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Ford v Ferrari',
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: const [
+                          Text(
+                            '2019',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            'PG-13',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            '2hr 32min',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: const Color(0xFFFE6D8E),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: const Icon(
+                        Icons.add,
+                        size: 15,
+                        color: Colors.grey,
+                      ))
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                children: [
+                  for (var item in list)
+                    Container(
+                      height: 35,
+                      width: 100,
+                      margin: const EdgeInsets.all(8),
+                      //padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey, width: 1.5),
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                          shape: BoxShape.rectangle),
+                      child: Center(
+                        child: Text(
+                          item,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Plot Summary',
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+            ),
+            SizedBox(height: 20,),
+            //RichText(text: InlineSpan)
           ],
         ),
       )),
